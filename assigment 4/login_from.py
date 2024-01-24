fe06b4b125e96e71ece681d1e9e6fa79c3238f81
webdev241 <mayursalunke241@gmail.com>
2024-01-24 08:19:41 +0530

    add python file

---
+++ assigment 4/login_from.py
@@ -0,0 +1,16 @@
+
+def login():
+    correct_username = "webdev"
+    correct_password = "pass123"
+
+    input_username = input("Enter your username: ")
+    input_password = input("Enter your password: ")
+
+    if input_username == correct_username and input_password == correct_password:
+        print("Login successful! Welcome, {}".format(correct_username))
+    else:
+        print("Login failed. Incorrect username or password.")
+
+print("Simple Login Application")
+
+login()
